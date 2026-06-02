f40ecfb81440dc7e363b89042bd4800d0ec199a3
Latko Artem <latko8973@gmail.com>
2026-06-03 00:40:34 +0300

    fix: add ssl and prepare:false for postgres.js on Supabase Cloud

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,5 +3,9 @@ import postgres from "postgres";
 import * as schema from "./schema";
 
 const connectionString = process.env.DATABASE_URL!;
-const client = postgres(connectionString);
+const isDocker = !!process.env.SUPABASE_INTERNAL_URL;
+const client = postgres(connectionString, {
+  prepare: false,
+  ...(!isDocker && { ssl: "require" }),
+});
 export const db = drizzle(client, { schema });
